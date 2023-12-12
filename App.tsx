c7ebbdbd19f02af77088b88ebadb607195532674
Nat <nathalie_sa@live.fr>
2023-12-12 17:46:19 +0100

    EditProfilPage+navigationHomePage/en attente page login pour redux

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-//import 'react-native-gesture-handler'; 
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
-
 //Navigation
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 //IMPÖRT DES SCREENS
 //For LOG
@@ -27,7 +23,6 @@ import NotificationScreen from '../frontend/screens/Profil/NotificationScreen';
 import PastOrderScreen from '../frontend/screens/Profil/PastOrderScreen';
 import ReportProblemScreen from '../frontend/screens/Profil/ReportProblemScreen';
 import SecurityScreen from '../frontend/screens/Profil/SecurityScreen';
-
 //A TRIER
 import BookDateScreen from './screens/BookDateScreen';
 import ConfigureOrderScreen from './screens/ConfigureOrderScreen';
@@ -36,87 +31,24 @@ import MainScreen from './screens/MainScreen';
 import OrderScreen from './screens/OrderScreen';
 //import ProfilScreen from './screens/Profil/ProfilScreen';
 
-
-
-
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 //REDUX
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from './reducers/user';
-//import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-
+//const navigation = useNavigation();
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
+//const Drawer = createDrawerNavigator();
 //const dispatch = useDispatch();
 
 
-const store = configureStore({
-  reducer: { user },
-})
+
 
 /*
-const NavigationContent = ({ isLoggedIn }) => {
-  //const user = useSelector((state) => state.user.value);
-  return (
-    <Provider store={store}> 
-    <NavigationContainer>
-
-      
-      {isLoggedIn ? (
-        <Tab.Navigator
-          
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName = '';
-
-              if (route.name === 'Home') {
-                iconName = 'home';
-              }
-               else if (route.name === 'Search') {
-                iconName = 'search';
-              } 
-              else if (route.name === 'Wishlist') {
-                iconName = 'heart';
-              }
-              return <FontAwesome name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: '#e8be4b',
-            tabBarInactiveTintColor: '#b2b2b2',
-            headerShown: false,
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Wishlist" component={WishlistScreen} />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Sign_up" component={SignUpScreen} />
-          <Stack.Screen name="Sign_in" component={SignInScreen} />
-        </Stack.Navigator>
-      )}
-
-      
-    </NavigationContainer>
-    </Provider>
-  );
-};
-
-const App = () => {
-  const userIsLoggedIn = false; // Remplacez ceci par votre logique d'authentification
-
-  return <NavigationContent isLoggedIn={userIsLoggedIn} />;
-};
-
-export default App; */
-
-
-
 
 const TabNavigatorForHome = () => {
   return (
@@ -143,40 +75,33 @@ const TabNavigatorForHome = () => {
       <Tab.Screen name="Wishlist" component={WishlistScreen}/>
     </Tab.Navigator>
   );
-};
+};*/
 
-const DrawerNavigatorForProfil = () => {
-  return (
-    <Drawer.Navigator screenOptions={{ headerShown: true }}>
-        <Drawer.Screen name="EditProfil" component={EditProfilScreen} />
-        <Drawer.Screen name="Notification" component={NotificationScreen} />
-        <Drawer.Screen name="Security" component={SecurityScreen} />
-        <Drawer.Screen name="PastOrder" component={PastOrderScreen} />
-        <Drawer.Screen name="HelpSupport" component={SupportScreen} />
-        <Drawer.Screen name="Problem" component={ReportProblemScreen}  />
-      </Drawer.Navigator>
 /*
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="EditProfil" component={EditProfilScreen} />
-        <Stack.Screen name="Notification" component={NotificationScreen} />
-        <Stack.Screen name="Security" component={SecurityScreen} />
-        <Stack.Screen name="PastOrder" component={PastOrderScreen} />
-        <Stack.Screen name="HelpSupport" component={SupportScreen} />
-        <Stack.Screen name="Problem" component={ReportProblemScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    */
+export default function Profil() {
+  return (
+    <Provider store={store}> 
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="EditProfil" component={EditProfilScreen} />
+          <Stack.Screen name="Notification" component={NotificationScreen} />
+          <Stack.Screen name="Security" component={SecurityScreen} />
+          <Stack.Screen name="PastOrder" component={PastOrderScreen} />
+          <Stack.Screen name="HelpSupport" component={SupportScreen} />
+          <Stack.Screen name="Problem" component={ReportProblemScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
-};
+}
+*/
 
+/*
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        
-        <Stack.Screen name="DrawerNavigatorForProfil" component={DrawerNavigatorForProfil} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Sign_up" component={SignUpScreen} />
         <Stack.Screen name="Sign_in" component={SignInScreen} />
@@ -184,5 +109,72 @@ export default function App() {
     </NavigationContainer>
   );
 }
+*/
+
+
+
+
+const HomeTabs = () => (
+  <Tab.Navigator screenOptions={({ route }) => ({
+    tabBarIcon: ({ color, size }) => {
+      let iconName: string = '';
+      if (route.name === 'Home') {
+        iconName = 'house';
+      } else if (route.name === 'Search') {
+        iconName = 'magnifying-glass';
+      } else if (route.name === 'Wishlist') {
+        iconName = 'heart';
+      }
+      return <FontAwesome name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: '#e8be4b',
+    tabBarInactiveTintColor: '#b2b2b2',
+    headerShown: false,
+  })}>
+    <Tab.Screen name="Main" component={MainScreen} />
+    <Tab.Screen name="Search" component={SearchScreen} />
+    <Tab.Screen name="Wishlist" component={WishlistScreen} />
+  </Tab.Navigator>
+);
+
+const store = configureStore({
+  reducer: { user },
+})
+
+
+//BESOIN:
+//dispatch(login({ username: signInUsername, token: data.token, id: data.id })); sur la page signin/signup
+export default function App() {
+  // Récupérer l'état d'authentification du store Redux
+  //const userToken = useSelector((state) => state.user.value.token|| null);
+  const userToken = false;
+
+  return (
+    <Provider store={store}> 
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {userToken ? (
+            <>
+              <Stack.Screen name="HomeTabs" component={HomeTabs} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Sign_in" component={SignInScreen} />
+              <Stack.Screen name="Sign_up" component={SignUpScreen} />
+            </>
+          )}
+          <Stack.Screen name="EditProfil" component={EditProfilScreen} />
+          <Stack.Screen name="Notification" component={NotificationScreen} />
+          <Stack.Screen name="Security" component={SecurityScreen} />
+          <Stack.Screen name="PastOrder" component={PastOrderScreen} />
+          <Stack.Screen name="HelpSupport" component={SupportScreen} />
+          <Stack.Screen name="Problem" component={ReportProblemScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
 
 
--- screens/Profil/EditProfilScreen.tsx
+++ screens/Profil/EditProfilScreen.tsx
@@ -1,11 +1,168 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function ProfilScreen() {
+interface Adresse {
+  rue: string;
+  ville: string;
+  codePostal: string;
+}
+
+interface UserProfil {
+  id: string,
+  nom: string;
+  prenom: string;
+  dateOfBirth: Date;
+  adresse: Adresse;
+  tel: string;
+  chef: boolean,
+  userConnexion: string,
+  orders: string[],
+  userPreference: string[],
+}
+
+export default function EditProfilScreen() {
+
+  const [ userProfil, setUserProfil ] =useState<UserProfil | null>(null);
+  //A RECUPERER DANS UN REDUCER
+  const [ userIdConnexion, setUserIdConnexion ] = useState<String | null>(null);
+  const [ modifCoordonne, setModifCoordonne ] = useState<boolean>(false);
+  const [ newRue, setNewRue ] = useState<string | null >("");
+  const [ newVille, setNewVille ] = useState<string | null>("");
+  const [ newCodePostal, setNewCodePostal ] = useState<string | null>("");
+  const [ newTel, setNewTel ] = useState<string | null>("");
+  const [ showAdresseMsg, setShowAdresseMsg] = useState<boolean>(false);
+
+  
+//recuperer les info de le BDD
+  const recupInfoUser = () => {
+    fetch(`http://localhost:3000/users/profil/${userIdConnexion}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.data);
+        setUserProfil(data.data);
+      })
+  }
+//recupère les info de le BDD au chargement de la page
+  useEffect(()=> {
+    recupInfoUser();
+  },[]);
+
+  const changeTel = () => {
+    fetch('http://localhost:3000/users')
+  };
+
+
+
+  let errorAdress = "";
+  const changeAdresse = () => {
+    setModifCoordonne(!modifCoordonne);
+    const userId = userProfil.id ;
+    const newAdresse = {
+      rue: newRue || userProfil.adresse.rue,
+      ville: newVille || userProfil.adresse.ville,
+      codePostal: newCodePostal || userProfil.adresse.codePostal
+    };
+    fetch(`http://localhost:3000/users/${userId}/update-adresse`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(newAdresse),
+    })
+      .then(res => res.json())
+      .then(data => {
+        if(data.result) {
+          //récupère les infos à jour de BDD pour les afficher
+          setShowAdresseMsg(false);
+          recupInfoUser();
+        } else { 
+          setShowAdresseMsg(true);
+          errorAdress = data.message;
+        }
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la requête POST :', error);
+      });
+  };
+
+  //InfoUser
+  const userDetails = userProfil? (
+    <View> 
+      <View>
+        <Text>Identité</Text>
+        <View> 
+          <Text>{userProfil.nom}</Text>
+          <Text>{userProfil.prenom}</Text>
+          <Text>{userProfil.dateOfBirth.toDateString()}</Text>
+        </View>
+        <TouchableOpacity activeOpacity={1} style={styles.btn_sign_in} >
+          <Text style={styles.buttonText_sign_in}>Edit</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <View> 
+          <Text>Coordonées</Text>
+          
+            { modifCoordonne ?
+            <View> 
+              <TextInput 
+                style={styles.input} 
+                placeholder={userProfil.adresse.rue}
+                onChangeText={(value) => setNewRue(value)} 
+                value={newRue || "" }/>
+              <TextInput 
+                style={styles.input} 
+                placeholder={userProfil.adresse.ville}
+                onChangeText={(value) => setNewVille(value)} 
+                value={newVille || "" }/>
+              <TextInput 
+                style={styles.input} 
+                placeholder={userProfil.adresse.codePostal}
+                onChangeText={(value) => setNewCodePostal(value)} 
+                value={newCodePostal || "" }/>
+                <Text style={styles.errorMsg}>* {errorAdress}</Text>
+              <TouchableOpacity activeOpacity={1} style={styles.btn_sign_up} onPress={()=> changeAdresse()} >
+                <Text style={styles.buttonText_sign_up}>Changer l'adresse</Text>
+              </TouchableOpacity>
+
+              <TextInput 
+                style={styles.input} 
+                placeholder={userProfil.tel}
+                onChangeText={(value) => setNewTel(value)} 
+                value={newTel || "" }/>
+              <TouchableOpacity activeOpacity={1} style={styles.btn_sign_up} onPress={()=> changeTel()} >
+                <Text style={styles.buttonText_sign_up}>Changer le numéro</Text>
+              </TouchableOpacity>
+            </View> :
+
+            <TouchableOpacity activeOpacity={1} style={styles.btn_sign_up} onPress={()=> setModifCoordonne(!modifCoordonne)}>
+              <Text style={styles.buttonText_sign_up}>{userProfil.adresse.rue}</Text>
+              <Text style={styles.buttonText_sign_up}>{userProfil.adresse.ville}</Text>
+              <Text style={styles.buttonText_sign_up}>{userProfil.adresse.codePostal}</Text>
+              <Text style={styles.buttonText_sign_up}>{userProfil.tel}</Text>
+            </TouchableOpacity>
+          }
+            
+          
+         
+
+         {/* <Text>{userProfil.adresse.rue}</Text>
+          <Text>{userProfil.adresse.ville}</Text>
+          <Text>{userProfil.adresse.codePostal}</Text>
+        <Text>Telephone: {userProfil.tel}</Text> */}
+        </View>
+        
+      </View>
+    </View>
+  ): null;
+
+
+
   return (
     <View style={styles.container}>
-      <Text>EDITPROFIL SCREEN</Text>
+      <Text>EDIT PROFIL SCREEN</Text>
+     {userDetails}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +175,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn_sign_in : {
+    paddingVertical: 10, // 10 units of padding at the top and bottom
+    paddingHorizontal: 25, // A
+    borderRadius: 5,
+    backgroundColor: '#9292FE',
+  },
+  buttonText_sign_in :  {
+    fontSize : 15,
+    color : '#fff'
+  },
+  btn_sign_up : {
+    paddingVertical: 10, // 10 units of padding at the top and bottom
+    paddingHorizontal: 25, // A
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#9292FE',
+    backgroundColor: '#fff',
+  },
+  buttonText_sign_up: {
+    fontSize : 15,
+    color : '#9292FE'
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: '#9292FE',
+    borderRadius:10,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  errorMsg: {
+    color: 'red',
+    fontSize: 10,
+  }
 });
