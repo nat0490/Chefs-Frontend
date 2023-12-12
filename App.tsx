13c2c87706bac50c8b8b69659b36c05ec521a6d6
Nat <nathalie_sa@live.fr>
2023-12-12 14:01:58 +0100

    création dossier profil

--- App.tsx
+++ App.tsx
@@ -1,25 +1,43 @@
+//import 'react-native-gesture-handler'; 
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 
+
+
+//Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 //IMPÖRT DES SCREENS
+//For LOG
+import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import LoginScreen from './screens/LoginScreen';
+//For Home
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import WishlistScreen from './screens/WishlistScreen';
+//For Profil
+import EditProfilScreen from '../frontend/screens/Profil/EditProfilScreen';
+import SupportScreen from '../frontend/screens/Profil/SupportScreen';
+import NotificationScreen from '../frontend/screens/Profil/NotificationScreen';
+import PastOrderScreen from '../frontend/screens/Profil/PastOrderScreen';
+import ReportProblemScreen from '../frontend/screens/Profil/ReportProblemScreen';
+import SecurityScreen from '../frontend/screens/Profil/SecurityScreen';
+
+//A TRIER
 import BookDateScreen from './screens/BookDateScreen';
 import ConfigureOrderScreen from './screens/ConfigureOrderScreen';
 import DishScreen from './screens/DishScreen';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
 import MainScreen from './screens/MainScreen';
 import OrderScreen from './screens/OrderScreen';
-import ProfilScreen from './screens/ProfilScreen';
-import SearchScreen from './screens/SearchScreen';
-import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import WishlistScreen from './screens/WishlistScreen';
+//import ProfilScreen from './screens/Profil/ProfilScreen';
+
+
+
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
@@ -32,6 +50,7 @@ import user from './reducers/user';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 //const dispatch = useDispatch();
 
 
@@ -39,12 +58,14 @@ const store = configureStore({
   reducer: { user },
 })
 
-
+/*
 const NavigationContent = ({ isLoggedIn }) => {
   //const user = useSelector((state) => state.user.value);
   return (
     <Provider store={store}> 
     <NavigationContainer>
+
+      
       {isLoggedIn ? (
         <Tab.Navigator
           
@@ -79,6 +100,8 @@ const NavigationContent = ({ isLoggedIn }) => {
           <Stack.Screen name="Sign_in" component={SignInScreen} />
         </Stack.Navigator>
       )}
+
+      
     </NavigationContainer>
     </Provider>
   );
@@ -90,12 +113,12 @@ const App = () => {
   return <NavigationContent isLoggedIn={userIsLoggedIn} />;
 };
 
-export default App;
+export default App; */
 
 
-/*
 
-const TabNavigator = () => {
+
+const TabNavigatorForHome = () => {
   return (
     <Tab.Navigator screenOptions={({ route }) => ({
       tabBarIcon: ({ color, size }) => {
@@ -121,12 +144,39 @@ const TabNavigator = () => {
     </Tab.Navigator>
   );
 };
-*/
+
+const DrawerNavigatorForProfil = () => {
+  return (
+    <Drawer.Navigator screenOptions={{ headerShown: true }}>
+        <Drawer.Screen name="EditProfil" component={EditProfilScreen} />
+        <Drawer.Screen name="Notification" component={NotificationScreen} />
+        <Drawer.Screen name="Security" component={SecurityScreen} />
+        <Drawer.Screen name="PastOrder" component={PastOrderScreen} />
+        <Drawer.Screen name="HelpSupport" component={SupportScreen} />
+        <Drawer.Screen name="Problem" component={ReportProblemScreen}  />
+      </Drawer.Navigator>
 /*
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="EditProfil" component={EditProfilScreen} />
+        <Stack.Screen name="Notification" component={NotificationScreen} />
+        <Stack.Screen name="Security" component={SecurityScreen} />
+        <Stack.Screen name="PastOrder" component={PastOrderScreen} />
+        <Stack.Screen name="HelpSupport" component={SupportScreen} />
+        <Stack.Screen name="Problem" component={ReportProblemScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    */
+  );
+};
+
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        
+        <Stack.Screen name="DrawerNavigatorForProfil" component={DrawerNavigatorForProfil} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Sign_up" component={SignUpScreen} />
         <Stack.Screen name="Sign_in" component={SignInScreen} />
@@ -135,4 +185,4 @@ export default function App() {
   );
 }
 
-*/
+
--- screens/Profil/EditProfilScreen.tsx
+++ screens/Profil/EditProfilScreen.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function ProfilScreen() {
   return (
     <View style={styles.container}>
-      <Text>PROFIL SCREEN</Text>
+      <Text>EDITPROFIL SCREEN</Text>
       <StatusBar style="auto" />
     </View>
   );
